5e76ee4d55f712faf3c2666eaad7031afaf88b8c
Chris Peters <peterschristopherryan@gmail.com>
2017-08-14 11:04:50 -0700

    did some stuff

---
+++ practice_banas/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace practice_banas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
